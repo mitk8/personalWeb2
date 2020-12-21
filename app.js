ea39cb6bc445634bc2bcc2ca64a178159e53c421
Андрей Митько <andrey@MacBook-Pro-5.local>
2020-12-21 20:40:54 +0300

    Remove spheres and add paralax effect to Hero Section

--- app.js
+++ app.js
@@ -25,3 +25,34 @@ function scrollAnchors(e, respond = null) {
         }
     }, 100);
 }
+
+
+//Movement Animation to happen
+const card = document.querySelector(".heroBlock");
+const container = document.querySelector(".heroSection");
+//Items
+const title = document.querySelector(".heroHeading");
+const description = document.querySelector(".heroTagline");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "all 0.1s ease";
+  //Popout
+  title.style.transform = "translateZ(100px)";
+  description.style.transform = "translateZ(50px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  description.style.transform = "translateZ(0px)";
+});
+
